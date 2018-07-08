6b5e1866bb9132ce20a469d67f4ea3a471f7dc9a
paul <paul.butcher@gmail.com>
2018-07-08 16:37:26 +0100

    add tests for location_in_grid

--- tests/test_contactsheet.py
+++ tests/test_contactsheet.py
@@ -37,3 +37,15 @@ def test_get_grid_size(cell_count):
     x, y = contactsheet.get_grid_size(cell_count)
     assert x * y >= cell_count
     assert abs(x - y) <= 1
+
+
+@pytest.mark.parametrize(
+    "grid, index, position",
+    [
+        ((3, 3), 0, (0, 0)),  # top-left
+        ((3, 3), 8, (2, 2)),  # bottom-right
+        ((4, 5), 6, (2, 1)),  # somewhere in the middle
+    ]
+)
+def test_get_location_in_grid(grid, index, position):
+    assert position == contactsheet.get_location_in_grid(grid, index)
